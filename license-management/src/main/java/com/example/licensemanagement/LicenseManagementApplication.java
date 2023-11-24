27d150261c5d002e18f2a46de2f7cff81cbbf1a9
Pavithra Sureshkumar <pasuit00@hs-esslingen.de>
2023-11-24 14:56:08 +0100

    Update to jakarta

--- license-management/src/main/java/com/example/licensemanagement/LicenseManagementApplication.java
+++ license-management/src/main/java/com/example/licensemanagement/LicenseManagementApplication.java
@@ -2,8 +2,10 @@ package com.example.licensemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.example.licensemanagement.Entity")
 public class LicenseManagementApplication {
 
 	public static void main(String[] args) {
