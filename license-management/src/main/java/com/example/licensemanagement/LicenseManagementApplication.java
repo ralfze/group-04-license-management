2bc4f3bf3d7270d10d74848f16ac356b01f0ab20
Pavithra Sureshkumar <pasuit00@hs-esslingen.de>
2023-11-24 14:09:15 +0100

    Create new Spring Boot Project

---
+++ license-management/src/main/java/com/example/licensemanagement/LicenseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.licensemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseManagementApplication.class, args);
+	}
+
+}
