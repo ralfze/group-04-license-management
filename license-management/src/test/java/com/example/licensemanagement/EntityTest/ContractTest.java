717600f8c8e9571a7b48a2b292070619c3b03584
Ralf Zeller <razeit01@hs-esslingen.de>
2023-12-02 20:19:15 +0100

    Update ContractTest

--- license-management/src/test/java/com/example/licensemanagement/EntityTest/ContractTest.java
+++ license-management/src/test/java/com/example/licensemanagement/EntityTest/ContractTest.java
@@ -1,5 +1,27 @@
 package com.example.licensemanagement.EntityTest;
 
-public class ContractTest {
+import org.junit.jupiter.api.Test;
+
+import com.example.licensemanagement.Entity.Contract;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContractTest {
+
+    @Test
+    void contractGetterSetterTest() {
+        // Create a contract instance
+        Contract contract = new Contract();
+        
+        // Set values using setters
+        contract.setId(1L);
+        // Set other properties...
+
+        // Test the getters
+        assertEquals(1L, contract.getId());
+        // Test other getters...
+
+    }
     
+    // Add more tests as needed
 }
