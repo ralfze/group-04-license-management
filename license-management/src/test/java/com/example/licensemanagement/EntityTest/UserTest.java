a613b83d0cba4e1146be260411b6cc90352d09ca
Ralf Zeller <razeit01@hs-esslingen.de>
2023-12-02 20:20:57 +0100

    Update UserTest #8

--- license-management/src/test/java/com/example/licensemanagement/EntityTest/UserTest.java
+++ license-management/src/test/java/com/example/licensemanagement/EntityTest/UserTest.java
@@ -1,5 +1,28 @@
+// User entity test
 package com.example.licensemanagement.EntityTest;
 
-public class UserTest {
+import org.junit.jupiter.api.Test;
+
+import com.example.licensemanagement.Entity.User;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void userGetterSetterTest() {
+        // Create a user instance
+        User user = new User();
+        
+        // Set values using setters
+        user.setId(1L);
+        // Set other properties...
+
+        // Test the getters
+        assertEquals(1L, user.getId());
+        // Test other getters...
+
+    }
     
+    // Add more tests as needed
 }
