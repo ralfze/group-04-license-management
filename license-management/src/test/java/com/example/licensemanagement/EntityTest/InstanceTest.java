316c9b147b2ad774cc9ca5ef3e3affbc3975f3c3
Ralf Zeller <razeit01@hs-esslingen.de>
2023-12-02 20:20:09 +0100

    Update InstanceText #8

--- license-management/src/test/java/com/example/licensemanagement/EntityTest/InstanceTest.java
+++ license-management/src/test/java/com/example/licensemanagement/EntityTest/InstanceTest.java
@@ -1,48 +1,25 @@
-package com.example.licensemanagement.Service;
+// Instance entity test
+package com.example.licensemanagement.Entity;
 
-import com.example.licensemanagement.Entity.Instance;
-import com.example.licensemanagement.Repo.InstanceRepository;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.*;
-
-class InstanceServiceTest {
-
-    @Mock
-    private InstanceRepository instanceRepository;
-
-    @InjectMocks
-    private InstanceService instanceService;
+class InstanceTest {
 
     @Test
-    void getAllInstancesTest() {
-        // Mock data
+    void instanceGetterSetterTest() {
+        // Create an instance instance
         Instance instance = new Instance();
-        List<Instance> instances = Arrays.asList(instance);
+        
+        // Set values using setters
+        instance.setId(1L);
+        // Set other properties...
 
-        // Mock repository behavior
-        when(instanceRepository.findAll()).thenReturn(instances);
+        // Test the getters
+        assertEquals(1L, instance.getId());
+        // Test other getters...
 
-        // Call the service method
-        List<Instance> result = instanceService.getAllInstances();
-
-        // Verify the result
-        assertNotNull(result);
-        assertEquals(1, result.size());
-        assertEquals(instance, result.get(0));
-
-        // Verify repository interactions
-        verify(instanceRepository, times(1)).findAll();
     }
-
-    // Add similar tests for other methods in InstanceService
+    
+    // Add more tests as needed
 }
