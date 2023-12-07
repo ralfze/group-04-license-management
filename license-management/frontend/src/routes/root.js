07d8cea2820de54c2775d20d0295e02cb4c776f3
Pavithra Sureshkumar <pasuit00@hs-esslingen.de>
2023-12-07 03:44:28 +0100

    Correct nav bar #4

--- license-management/frontend/src/routes/root.js
+++ license-management/frontend/src/routes/root.js
@@ -10,6 +10,8 @@ import {
   Avatar,
   Grid,
   Heading,
+  Flex,
+  Spacer,
 } from '@chakra-ui/react';
 import { IconLogout } from '@tabler/icons-react';
 import { Link } from 'react-router-dom';
@@ -17,62 +19,64 @@ import { Link } from 'react-router-dom';
 const Root = () => {
   return (
     <ChakraProvider>
-      <Box id="sidebar">
-        <VStack>
-          <HStack>
-            <h1>License Management</h1>
-            <Button as={Link} to="/add" variant="solid" size="md">
-              Add
-            </Button>
-            <Input size="md" placeholder="filter"></Input>
-            <Avatar size="md"></Avatar>
-            <Button as={Link} to="/add" variant="solid" size="md">
-              <IconLogout size="4em" />
-            </Button>
-          </HStack>
-          <Grid spacing="0">
-            <Box w="20%">
-              <Stack spacing={4} flexDirection="column">
-                <Button as={Link} to="/customers" variant="solid" size="md">
-                  Customers
-                </Button>
-                <Button as={Link} to="/contracts" variant="solid" size="md">
-                  Contracts
-                </Button>
-                <Button as={Link} to="/instances" variant="solid" size="md">
-                  Instances
-                </Button>
-                <Button as={Link} to="/users" variant="solid" size="md">
-                  Users
-                </Button>
-              </Stack>
-            </Box>
+      <Flex as='nav-top' p='1em' alignItems='center'>
+        <Heading as='h1'>License Management</Heading>
+        <Spacer />
 
-            <VStack w="80%">
-              {
-                //<!-- Content -->
-                <HStack>
-                  <Heading>Customer</Heading>
-                  <Heading> Address a</Heading>
-                  <Heading> Address b</Heading>
-                  <Button as={Link} to="edit" variant="solid" size="md">
-                  Edit
-                </Button>
-                <Button as={Link} to="/delete" variant="solid" size="md">
-                  Delete
-                </Button>
-                <Button as={Link} to="/contracts" variant="solid" size="md">
-                  Contracts
-                </Button>
-                <Button as={Link} to="/users" variant="solid" size="md">
-                  Users
-                </Button>
-                </HStack>
-              }
-            </VStack>
-          </Grid>
+        <HStack spacing='1em'>
+          <Button as={Link} to='/add' variant='solid' size='md'>
+            Add
+          </Button>
+          <Input placeholder='filter'></Input>
+          <Avatar ></Avatar>
+          <Button as={Link} to='/add' variant='solid' >
+            <IconLogout size='4em' />
+          </Button>
+        </HStack>
+      </Flex>
+      
+
+      <Grid spacing='0'>
+        <Box w='20%'>
+          <Stack spacing={4} flexDirection='column'>
+            <Button as={Link} to='/customers' variant='solid' size='md'>
+              Customers
+            </Button>
+            <Button as={Link} to='/contracts' variant='solid' size='md'>
+              Contracts
+            </Button>
+            <Button as={Link} to='/instances' variant='solid' size='md'>
+              Instances
+            </Button>
+            <Button as={Link} to='/users' variant='solid' size='md'>
+              Users
+            </Button>
+          </Stack>
+        </Box>
+
+        <VStack w='80%'>
+          {
+            //<!-- Content -->
+            <HStack>
+              <Heading>Customer</Heading>
+              <Heading> Address a</Heading>
+              <Heading> Address b</Heading>
+              <Button as={Link} to='edit' variant='solid' size='md'>
+                Edit
+              </Button>
+              <Button as={Link} to='/delete' variant='solid' size='md'>
+                Delete
+              </Button>
+              <Button as={Link} to='/contracts' variant='solid' size='md'>
+                Contracts
+              </Button>
+              <Button as={Link} to='/users' variant='solid' size='md'>
+                Users
+              </Button>
+            </HStack>
+          }
         </VStack>
-      </Box>
+      </Grid>
     </ChakraProvider>
   );
 };
