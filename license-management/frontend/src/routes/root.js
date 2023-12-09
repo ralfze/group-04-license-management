ac64e363d0c99ddebab43f4847d165ad1c45a3e8
Pavithra Sureshkumar <pasuit00@hs-esslingen.de>
2023-12-09 03:01:25 +0100

    Correct flex of UI #4

--- license-management/frontend/src/routes/root.js
+++ license-management/frontend/src/routes/root.js
@@ -15,11 +15,11 @@ import { Link } from 'react-router-dom';
 const Root = () => {
   return (
     <ChakraProvider>
-      <Flex as="div" p="1em" alignItems="center">
+      <Flex as="div" p="1em" alignItems="center" direction="row">
         <Heading as="h1">License Management</Heading>
         <Spacer />
 
-        <HStack spacing="1em">
+        <HStack spacing="1em" >
           <Button as={Link} to="/add" variant="solid" size="md">
             Add
           </Button>
@@ -31,11 +31,13 @@ const Root = () => {
         </HStack>
       </Flex>
 
-      <Flex as="nav" p="1em" alignItems="flex-start">
-        <Flex spacing="1em" direction="column">
+      <Flex as="nav" p="1em" direction="row">
+        <Flex spacing="1em" direction="column" flex="1">
+
           <Button as={Link} to="/customers" variant="solid" size="md">
             Customers
           </Button>
+
           <Button as={Link} to="/contracts" variant="solid" size="md">
             Contracts
           </Button>
@@ -47,63 +49,63 @@ const Root = () => {
           </Button>
         </Flex>
 
-        <Flex as="div" direction='column' paddingTop='0em' paddingLeft='1em' gap='0.5em'>
-          <HStack w='100%'>
-            <Heading as="h2" size="md">
+        <Flex as="div" direction='column' paddingTop='0em' paddingLeft='1em' gap='0.5em' flex="9">
+          <HStack flex="1">
+            <Heading as="h2" size="md" flex="1">
               Customer
             </Heading>
 
-            <Heading as="h2" size="md">
+            <Heading as="h2" size="md" flex="1">
               Address a
             </Heading>
 
-            <Heading as="h2" size="md">
+            <Heading as="h2" size="md" flex="1">
               Address b
             </Heading>
 
-            <Button as={Link} to="edit" variant="solid" size="md">
+            <Button as={Link} to="edit" variant="solid" size="md" flex="1">
               Edit
             </Button>
 
-            <Button as={Link} to="/delete" variant="solid" size="md">
+            <Button as={Link} to="/delete" variant="solid" size="md" flex="1">
               Delete
             </Button>
 
-            <Button as={Link} to="/contracts" variant="solid" size="md">
+            <Button as={Link} to="/contracts" variant="solid" size="md" flex="1">
               Contracts
             </Button>
 
-            <Button as={Link} to="/users" variant="solid" size="md">
+            <Button as={Link} to="/users" variant="solid" size="md" flex="1">
               Users
             </Button>
           </HStack>
 
-          <HStack w='100%'>
-            <Heading as="h2" size="md">
+          <HStack flex="1">
+            <Heading as="h2" size="md" flex="1">
               Customer
             </Heading>
 
-            <Heading as="h2" size="md">
+            <Heading as="h2" size="md" flex="1">
               Address a
             </Heading>
 
-            <Heading as="h2" size="md">
+            <Heading as="h2" size="md" flex="1">
               Address b
             </Heading>
 
-            <Button as={Link} to="edit" variant="solid" size="md">
+            <Button as={Link} to="edit" variant="solid" size="md" flex="1">
               Edit
             </Button>
 
-            <Button as={Link} to="/delete" variant="solid" size="md">
+            <Button as={Link} to="/delete" variant="solid" size="md" flex="1">
               Delete
             </Button>
 
-            <Button as={Link} to="/contracts" variant="solid" size="md">
+            <Button as={Link} to="/contracts" variant="solid" size="md" flex="1">
               Contracts
             </Button>
 
-            <Button as={Link} to="/users" variant="solid" size="md">
+            <Button as={Link} to="/users" variant="solid" size="md" flex="1">
               Users
             </Button>
           </HStack>
