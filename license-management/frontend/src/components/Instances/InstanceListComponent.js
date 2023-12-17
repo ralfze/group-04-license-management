343a39e8c058bc5fc2ff7b38cb5cebb8d47bec68
Pavithra Sureshkumar <pasuit00@hs-esslingen.de>
2023-12-17 09:51:35 +0100

    Add InstanceListComponent #13

---
+++ license-management/frontend/src/components/Instances/InstanceListComponent.js
@@ -0,0 +1,77 @@
+// frontend/src/components/Instances/InstanceListComponent.js
+import React, { useState, useEffect } from 'react';
+import { Button, HStack, Heading, Flex } from '@chakra-ui/react';
+import { Link } from 'react-router-dom'; // Import Link and useNavigate
+import InstanceService from '../../services/InstanceService';
+
+const InstanceListComponent = ({ navigate }) => {
+  const [instances, setInstances] = useState([]);
+
+  //const navigate = useNavigate(); // Get the navigate function from the hook
+
+  useEffect(() => {
+    loadInstances();
+  }
+    , [navigate]);
+
+  const loadInstances = async () => {
+    try {
+      const response = await InstanceService.getAllInstances();
+      setInstances(response.data);
+      //console.log(response.data);
+    } catch (error) {
+      console.error('Error loading Instances:', error);
+    }
+  };
+
+  return (
+    <Flex
+      as="div"
+      direction="column"
+      paddingTop="0em"
+      paddingLeft="1em"
+      gap="0.5em"
+      flex="9"
+    >
+      {instances.length > 0 ? (
+        instances.map((instance) => (
+          <HStack flex="1" key={instance.id}>
+            (instance.contract.customer &&
+            <Heading as="h2" size="md" flex="2" key={`custName-${instance.id}`}>
+              {instance.contract.customer.name}
+            </Heading>
+            )
+            <Heading as="h2" size="md" flex="2" key={`instName-${instance.id}`}>
+              {instance.name}
+            </Heading>
+            (instance.contract && (
+            <Heading as="h2" size="md" flex="2" key={`startDate-${instance.id}`}>
+              {instance.contract.startDate}
+            </Heading>
+
+            <Heading as="h2" size="md" flex="2" key={`endDate-${instance.id}`}>
+              {instance.contract.endDate}
+            </Heading>)
+            )
+            <Button as={Link} to={`edit/${instance.id}`} variant="solid" size="md" flex="1" key={`edit-${Instance.id}`}>
+              Edit
+            </Button>
+
+            <Button as={Link} to={`delete/${instance.id}`} variant="solid" size="md" flex="1" key={`delete-${Instance.id}`}>
+              Delete
+            </Button>
+
+            <Button as={Link} to={`details/${instance.id}`} variant="solid" size="md" flex="1" key={`details-${Instance.id}`}>
+              Details
+            </Button>
+          </HStack>
+        ))) : (
+        <p>No Instances available</p>
+      )}
+
+    </Flex>
+
+  );
+};
+
+export default InstanceListComponent;
--- license-management/frontend/src/routes/InstanceScreen.js
+++ license-management/frontend/src/routes/InstanceScreen.js
@@ -12,6 +12,7 @@ import {
 } from '@chakra-ui/react';
 import { IconLogout } from '@tabler/icons-react';
 import { Link } from 'react-router-dom';
+import InstanceListComponent from '../components/Instances/InstanceListComponent';
 
 const InstanceScreen = () => {
   return (
@@ -57,144 +58,7 @@ const InstanceScreen = () => {
             Users
           </Button>
         </Flex>
-
-        <Flex
-          as="div"
-          direction="column"
-          paddingTop="0em"
-          paddingLeft="1em"
-          gap="0.5em"
-          flex="9"
-        >
-          <HStack flex="1" alignItems="start">
-            <Heading as="h2" size="md" flex="0" align="flex-start">
-              Customer
-            </Heading>
-
-            <VStack flex="1">
-              <HStack flex="1">
-
-              <Heading as="h2" size="md" flex="1">
-                  Instance BA
-                </Heading>
-
-                <Heading as="h2" size="md" flex="1">
-                  Contract start
-                </Heading>
-
-                <Heading as="h2" size="md" flex="1">
-                  Contract end
-                </Heading>
-
-                
-
-                <Button as={Link} to="edit" variant="solid" size="md" flex="1">
-                  Edit
-                </Button>
-
-                <Button
-                  as={Link}
-                  to="/delete"
-                  variant="solid"
-                  size="md"
-                  flex="1"
-                >
-                  Delete
-                </Button>
-
-                <Button
-                  as={Link}
-                  to="/contracts"
-                  variant="solid"
-                  size="md"
-                  flex="1"
-                >
-                  Details
-                </Button>
-              </HStack>
-              <HStack flex="1">
-
-              <Heading as="h2" size="md" flex="1">
-                  Instance BB
-                </Heading>
-
-                <Heading as="h2" size="md" flex="1">
-                  Contract start
-                </Heading>
-
-                <Heading as="h2" size="md" flex="1">
-                  Contract end
-                </Heading>
-
-                
-
-                <Button as={Link} to="edit" variant="solid" size="md" flex="1">
-                  Edit
-                </Button>
-
-                <Button
-                  as={Link}
-                  to="/delete"
-                  variant="solid"
-                  size="md"
-                  flex="1"
-                >
-                  Delete
-                </Button>
-
-                <Button
-                  as={Link}
-                  to="/contracts"
-                  variant="solid"
-                  size="md"
-                  flex="1"
-                >
-                  Details
-                </Button>
-              </HStack>
-              <HStack flex="1">
-
-              <Heading as="h2" size="md" flex="1">
-                  Instance BC
-                </Heading>
-
-                <Heading as="h2" size="md" flex="1">
-                  Contract start
-                </Heading>
-
-                <Heading as="h2" size="md" flex="1">
-                  Contract end
-                </Heading>
-
-                
-
-                <Button as={Link} to="edit" variant="solid" size="md" flex="1">
-                  Edit
-                </Button>
-
-                <Button
-                  as={Link}
-                  to="/delete"
-                  variant="solid"
-                  size="md"
-                  flex="1"
-                >
-                  Delete
-                </Button>
-
-                <Button
-                  as={Link}
-                  to="/contracts"
-                  variant="solid"
-                  size="md"
-                  flex="1"
-                >
-                  Details
-                </Button>
-              </HStack>
-            </VStack>
-          </HStack>
-        </Flex>
+        <InstanceListComponent />
       </Flex>
     </Box>
   );
