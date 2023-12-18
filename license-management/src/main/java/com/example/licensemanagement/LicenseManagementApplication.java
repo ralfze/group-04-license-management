4912821e5f24dce4ca06869c9d682e7899a2c434
Ralf Zeller <razeit01@hs-esslingen.de>
2023-12-18 21:04:18 +0100

    Fix some problem #17

--- license-management/src/main/java/com/example/licensemanagement/LicenseManagementApplication.java
+++ license-management/src/main/java/com/example/licensemanagement/LicenseManagementApplication.java
@@ -1,6 +1,5 @@
 package com.example.licensemanagement;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
